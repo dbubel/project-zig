ea31d77fc1b3271f526a2b5a477b086475db258e
dbubel <50341559+dbubel@users.noreply.github.com>
2023-10-18 21:51:12 -0400

    stuff double

--- double_linked.zig
+++ double_linked.zig
@@ -17,23 +17,69 @@ pub const linked_list = struct {
 
     pub fn pushFront(self: *linked_list, value: []u8) !void {
         var new_node: *Node = try self.allocator.create(Node);
-        new_node.* = Node{ .value = value, .next = self.head };
+        new_node.* = Node{ .value = value, .prev = null, .next = self.head };
         self.head = new_node;
     }
 
+    pub fn pushBack(self: *linked_list, value: []u8) !void {
+        var new_node: *Node = try self.allocator.create(Node);
+        new_node.* = Node{ .value = value, .prev = self.tail, .next = null };
+        // std.debug.print("new node {any}\n", .{new_node});
+        self.tail = new_node;
+        if (self.head) |head| {
+            _ = head;
+            // std.debug.print("adding head {any}\n", .{head});
+            // self.head = new_node;
+        } else {
+            // std.debug.print("not adding head\n", .{});
+            self.head = new_node;
+            // std.debug.print("adding head {any} {any}\n", .{ self.head, new_node });
+        }
+    }
+
+    pub fn printHead(self: *linked_list) void {
+        std.debug.print("HEAD {any}\n", .{self.head});
+    }
     pub fn print(self: *linked_list) void {
         var current_node: ?*Node = self.head;
         while (current_node) |i| {
-            std.debug.print("{d}\n", .{i.value});
+            std.debug.print("{d} {any} {any}\n", .{ i.value, i.next, i.prev });
             current_node = i.next;
         }
+    }
+
+    pub fn removeAll(self: *linked_list) void {
+        var current_node: ?*Node = self.head;
+        while (current_node) |i| {
+            current_node = i.next;
+
+            self.allocator.destroy(i);
+        }
+        self.head = null;
     }
 };
 
 test "diff allocator" {
-    var arena = std.testing.allocator;
-    // defer arena.deinit();
-    var allocator = arena;
+    // var arena = std.testing.allocator;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    std.debug.print("\n", .{});
+    var allocator = arena.allocator();
     var ll: linked_list = linked_list.init(&allocator);
-    _ = try ll.pushFront(1337);
+    var x = [_]u8{1};
+    var y = [_]u8{2};
+    var z = [_]u8{3};
+    // _ = try ll.pushFront(&x);
+    // _ = try ll.pushFront(&y);
+    // _ = try ll.pushFront(&z);
+    var i = [_]u8{4};
+    _ = i;
+    _ = try ll.pushBack(&x);
+    ll.printHead();
+    _ = try ll.pushBack(&y);
+    ll.printHead();
+    _ = try ll.pushBack(&z);
+    ll.printHead();
+    ll.print();
+    ll.removeAll();
 }
